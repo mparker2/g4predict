19f3bdecc94b57c093712af7ae23d17f928d0db7
Matthew Parker <mparker2@sheffield.ac.uk>
2016-10-18 14:11:45 +0100

    pep8

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
-setup(name='g4predict',
-      version='0.1',
-      description=(
-          'Predict putative G Quadruplexes using an extension'
-          'of the Quadparser method'
-      ),
-      author='Matthew Parker',
-      scripts=[
-          'scripts/g4predict.py'
-      ],
-      packages=[
-          'g4funcs',
-      ],
-      install_requires=[
-          'regex>=2016.3.2'
-      ]
+setup(
+    name='g4predict',
+    version='0.1',
+    description=(
+        'Predict putative G Quadruplexes using an extension'
+        'of the Quadparser method'
+    ),
+    author='Matthew Parker',
+    scripts=[
+        'scripts/g4predict.py'
+    ],
+    packages=[
+        'g4funcs',
+    ],
+    install_requires=[
+        'regex>=2016.3.2'
+    ]
 )
